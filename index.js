151417bb3edd60c7c7514ab9d8859aa25b73637d
leonard-pointe <paulostrander@proton.me>
2025-10-19 10:37:58 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const posts = [
         name: "Vincent van Gogh",
         username: "vincey1853",
         location: "Zundert, Netherlands",
-        avatar: "images/avatar-vangogh.jpg",
-        post: "images/post-vangogh.jpg",
+        avatar: "/images/avatar-vangogh.jpg",
+        post: "/images/post-vangogh.jpg",
         comment: "just took a few mushrooms lol",
         likes: 21,
         liked: false
@@ -13,8 +13,8 @@ const posts = [
         name: "Gustave Courbet",
         username: "gus1819",
         location: "Ornans, France",
-        avatar: "images/avatar-courbet.jpg",
-        post: "images/post-courbet.jpg",
+        avatar: "/images/avatar-courbet.jpg",
+        post: "/images/post-courbet.jpg",
         comment: "i'm feelin a bit stressed tbh",
         likes: 4,
         liked: false
@@ -23,8 +23,8 @@ const posts = [
         name: "Joseph Ducreux",
         username: "jd1735",
         location: "Paris, France",
-        avatar: "images/avatar-ducreux.jpg",
-        post: "images/post-ducreux.jpg",
+        avatar: "/images/avatar-ducreux.jpg",
+        post: "/images/post-ducreux.jpg",
         comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
         likes: 152,
         liked: false
@@ -50,9 +50,9 @@ for (let i = 0; i < posts.length; i++) {
             </section>
             <section class="post-info">
                 <div class="interactions">
-                    <img src="images/icon-heart.png" class="btn btn-heart" id="heart-${i}">
-                    <img src="images/icon-comment.png" class="btn" id="comment-${i}">
-                    <img src="images/icon-dm.png" class="btn">
+                    <img src="/images/icon-heart.png" class="btn btn-heart" id="heart-${i}">
+                    <img src="/images/icon-comment.png" class="btn" id="comment-${i}">
+                    <img src="/images/icon-dm.png" class="btn">
                 </div>
                 <div>
                     <p class="bold" id="likes-${i}">${post.likes} likes</p>
